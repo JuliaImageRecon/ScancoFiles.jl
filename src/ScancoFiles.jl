57c372ffe8adfb849853f80743e28adf70de3c55
Tobias Knopp <tobias.knopp@tuhh.de>
2022-12-23 00:01:09 +0100

    initial commit

--- src/ScancoFiles.jl
+++ src/ScancoFiles.jl
@@ -1,5 +1,187 @@
 module ScancoFiles
 
-# Write your package code here.
+using ImageMetadata
+using Unitful: cm
+
+# see also https://github.com/KitwareMedical/ITKIOScanco/blob/master/src/itkScancoImageIO.cxx
+
+export loadISQ, loadRSQ
+
+function readScancoInt(fd)
+  return read(fd, Int32)
+end
+
+function readScancoFloat32(fd)
+  return read(fd, Float32)
+end
+
+function readScancoFloat64(fd)
+  return read(fd, Float64)
+end
+
+function readScancoString(fd, len)
+  return String( read!(fd, Array{UInt8}(undef, len)) )
+end
+
+
+function load(filename::AbstractString; kargs)
+  filenamebase, ext = splitext(filename)
+
+  if lowercase(ext) == ".isq"
+    return loadISQ(filename; kargs...)
+  elseif lowercase(ext) == ".rsq"
+    return loadRSQ(filename; kargs...)
+  else
+    error("cannot read $(ext) files")
+  end
+end
+
+function loadISQ(filename::AbstractString)
+  filenamebase, ext = splitext(filename)
+
+  I = open(filename, "r")  do fd
+    params = Dict{String, Any}()
+    params["version"] = readScancoString(fd, 16)
+    params["dataType"] = readScancoInt(fd)
+    params["numBytes"] = readScancoInt(fd)
+    params["numBlocks"] = readScancoInt(fd)
+    params["patientIndex"] = readScancoInt(fd)
+    params["scannerID"] = readScancoInt(fd)
+    # TODO read date correctly
+    read!(fd, Array{UInt8}(undef, 8))
+    params["pixdim"] = ntuple(_->readScancoInt(fd), 3)
+    params["physdim"] = ntuple(_->readScancoInt(fd)*1e-4cm, 3)
+
+    isRAD = (params["dataType"] == 9 || params["physdim"][3] == 0)
+    if isRAD
+      error("cannot read rad files")
+    end
+
+    params["sliceThickness"] = readScancoInt(fd)*1e-3
+    params["sliceIncrement"] = readScancoInt(fd)*1e-3
+    params["startPosition"] = readScancoInt(fd)*1e-3
+    # this->m_EndPosition = this->m_StartPosition + physdim[2] * 1e-3 * (pixdim[2] - 1) / pixdim[2];
+    params["dataRange"] = ntuple(_->readScancoInt(fd), 2)
+    params["muScaling"] = readScancoInt(fd)
+    params["numberOfSamples"] = readScancoInt(fd)
+    params["numberOfProjections"] = readScancoInt(fd)
+    params["scanDistance"] = readScancoInt(fd)*1e-3
+    params["scannerType"] = readScancoInt(fd)
+    params["sampleTime"] = readScancoInt(fd)*1e-3
+    params["measurementIndex"] = readScancoInt(fd)
+    params["site"] = readScancoInt(fd)
+    params["referenceLine"] = readScancoInt(fd)*1e-3
+    params["reconstructionAlg"] = readScancoInt(fd)
+    params["patientName"] = strip(readScancoString(fd, 40))
+    params["energy"] = readScancoInt(fd)*1e-3
+    params["intensity"] = readScancoInt(fd)*1e-3
+
+    seek(fd, 508)
+
+    params["dataOffset"] = readScancoInt(fd)
+
+    params["headerSize"] = (params["dataOffset"] + 1) * 512
+
+    seek(fd, params["headerSize"])
+
+    dtype = Int16 # TODO: match this
+    I = read!(fd, Array{dtype}(undef, params["pixdim"]))
+    
+    # TODO: Scale I
+    return ImageMeta(I, ImageMetadata.to_dict(params))
+  end
+  
+  return I
+end
+
+
+function loadRSQ(filename::AbstractString; sinogramPreprocessing=true)
+  filenamebase, ext = splitext(filename)
+  
+  I = open(filename, "r")  do fd
+    params = Dict{String, Any}()
+    params["version"] = readScancoString(fd, 16)
+    params["dataType"] = readScancoInt(fd)
+    params["numBytes"] = readScancoInt(fd)
+    params["numBlocks"] = readScancoInt(fd)
+    params["patientIndex"] = readScancoInt(fd)
+    params["scannerID"] = readScancoInt(fd)
+    # TODO read date correctly
+    read!(fd, Array{UInt8}(undef, 8))
+    params["pixdim"] = ntuple(_->readScancoInt(fd), 3)
+    params["physdim"] = ntuple(_->readScancoInt(fd)*1e-4cm, 3)
+
+    isRAD = (params["dataType"] ==9 || params["physdim"][3] == 0)
+    if isRAD
+      error("cannot read rad files")
+    end
+
+    params["sliceThickness"] = readScancoInt(fd)*1e-3
+    params["sliceIncrement"] = readScancoInt(fd)*1e-3
+    params["startPosition"] = readScancoInt(fd)*1e-3
+    # this->m_EndPosition = this->m_StartPosition + physdim[2] * 1e-3 * (pixdim[2] - 1) / pixdim[2];
+    params["dataRange"] = ntuple(_->readScancoInt(fd), 2)
+    params["muScaling"] = readScancoInt(fd)
+    params["numberOfSamples"] = readScancoInt(fd)
+    params["numberOfProjections"] = readScancoInt(fd)
+    params["detectorWidth"] = readScancoInt(fd)*1e-4cm
+    params["detectorHeight"] = readScancoInt(fd)*1e-4cm
+    params["centerPixel"] = ntuple(_->readScancoInt(fd)*1e-4cm, 2)
+    params["detectorSourceDist"] = readScancoInt(fd)*1e-4cm
+    params["detectorCenterDist"] = params["detectorSourceDist"] - readScancoInt(fd)*1e-4cm
+    readScancoInt(fd)
+    read!(fd, Array{UInt8}(undef, 12 * 4))
+    params["dataPixelR"] = readScancoInt(fd)
+    readScancoInt(fd)
+    readScancoInt(fd)
+    params["ioRecord"] = readScancoInt(fd)
+    params["darkRecord"] = readScancoInt(fd)
+    params["dataRecord"] = readScancoInt(fd)
+    readScancoInt(fd)
+    params["integrationTime"] = readScancoInt(fd)
+    params["patientName"] = strip(readScancoString(fd, 40))
+    params["energy"] = readScancoInt(fd)
+    params["intensity"] = readScancoInt(fd)
+
+    seek(fd, 480)
+    params["orbitStartOffset"] = readScancoInt(fd)*1e-3
+
+    seek(fd, 508)
+    params["dataOffset"] = readScancoInt(fd)
+
+    params["headerSize"] = (params["dataOffset"] + 1) * 512
+
+    seek(fd, params["headerSize"])
+
+    dtype = Int16 
+    I = read!(fd, Array{dtype}(undef, params["pixdim"]))
+    
+    # derived parameters
+    params["detectorOffset"] = 0      
+    d = params["detectorWidth"] / size(I,1) 
+    a = sqrt((2*params["detectorSourceDist"])^2 - params["detectorWidth"]^2)*0.5
+    β = 2*asin(params["detectorWidth"]/(2*a))/pi*180 
+    params["fanAngle"] = β
+    params["angleRange"] = 180+params["fanAngle"] #γ
+
+    params["calibrationScans"] = I[:,1:2,:]
+    I = I[:,3:end,:]
+    reverse!(I, dims=2)
+
+    if sinogramPreprocessing
+      C = params["calibrationScans"]
+      I_ = similar(I, Float32)
+      for l=1:size(I,3)
+        I_[:,:,l] = (-log.((I[:,:,l] .- C[:,1,l]) ./ (C[:,2,l] .- C[:,1,l]))) * 2^13 # magic factor
+      end
+      I = I_
+    end
+
+    return ImageMeta(I, ImageMetadata.to_dict(params))
+  end
+ 
+  return I
+end
+
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,47 @@
 using ScancoFiles
 using Test
+using Scratch
+using Unitful: cm
+
+const datadir  = @get_scratch!("ScancoFiles")
+@info "If you want to check the output of the tests, please head to $datadir."
+
+filenameISQ = joinpath(datadir, "C0004255.ISQ")
+if !isfile(filenameISQ)
+  download("https://data.kitware.com/api/v1/file/591e56178d777f16d01e0d20/download", filenameISQ)
+end
+@info filenameISQ
 
 @testset "ScancoFiles.jl" begin
-    # Write your tests here.
+    img = loadISQ(filenameISQ)
+
+    @test img.version == "CTDATA-HEADER_V1"
+    @test size(img) ==  (1024, 1024, 51)
+    @test img.scannerID == 2135
+    @test img.numberOfProjections == 500
+    @test img.scannerType == 10
+    @test img.intensity == 0.177
+    @test img.dataRange == (-2813, 32767)
+    @test img.pixdim == (1024, 1024, 51)
+    @test img.reconstructionAlg == 3
+    @test img.sliceThickness == 0.036000000000000004
+    @test img.sliceIncrement == 0.036000000000000004
+    @test img.measurementIndex == 4937
+    @test img.referenceLine == 0.0
+    @test img.site == 5
+    @test img.dataOffset == 6
+    @test img.patientName == "COLE-BPBP"                               
+    @test img.numberOfSamples == 1024
+    @test img.sampleTime == 400.0
+    @test img.muScaling == 4096
+    @test all(img.physdim .≈ (3.6864, 3.6864, 0.1836).*cm)
+    @test img.numBlocks == 208903
+    @test img.energy == 45.0
+    @test img.dataType == 3
+    @test img.headerSize == 3584
+    @test img.scanDistance == 36.864000000000004
+    @test img.startPosition == 75.0
+    @test img.numBytes == 106958336
+    @test img.patientIndex == 78
+
 end
